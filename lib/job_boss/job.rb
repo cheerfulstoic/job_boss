2f7b9181fc5aafe370afb12f9c0fc535e9fa0192
Brian Underwood <bunderwood@rbmtechnologies.com>
2010-11-26 14:30:18 -0500

    Use fork instead of spawn (don't want to deal with Rails dependency).  Also, working on public interface of Job class

--- lib/job_boss/job.rb
+++ lib/job_boss/job.rb
@@ -14,9 +14,11 @@ module JobBoss
     def dispatch
       self.mark_as_started
       puts "Dispatching Job ##{self.id}"
-      require 'spawn'
 
-      spawn(:nice => 19) do
+      fork do
+        self.mark_employee
+        Process.setpriority(Process::PRIO_PROCESS, 0, 19)
+
         begin
           $0 = "job_employee (job ##{self.id})"
 
@@ -28,58 +30,83 @@ module JobBoss
           self.update_attribute(:result, result)
         rescue Exception => exception
           self.mark_exception(exception)
+          puts "Error running job ##{self.id}!"
         ensure
           self.mark_as_completed
+          puts "Job ##{self.id} completed, exiting..."
           Kernel.exit
         end
       end
     end
 
-    def mark_as_started
-      require 'socket'
-      update_attributes(:started_at       => Time.now,
-                        :employee_host    => Socket.gethostname,
-                        :employee_pid     => Process.pid)
-    end
-
-    def mark_exception(exception)
-      update_attributes(:status => :error, :error_message => exception.message, :error_backtrace => exception.backtrace)
-    end
-
-    def mark_as_completed
-      self.status ||= :success
-      self.completed_at = Time.now
-      self.save
-    end
-
     def mark_for_redo
-      update_attributes(:started_at => nil, :completed_at => nil, :status => nil, :error_message => nil, :error_backtrace => nil)
+      self.reload
+      self.started_at = nil
+      self.result = nil
+      self.completed_at = nil
+      self.status = nil
+      self.error_message = nil
+      self.error_backtrace = nil
+      self.employee_host = nil
+      self.employee_pid = nil
+      self.save
     end
 
     def completed?
       !!completed_at
     end
 
-    class << self
-      def call_path(path, *args)
-        require 'active_support'
+    def succeeded?
+      completed_at && (status == 'success')
+    end
 
-        raise ArgumentError, "Invalid path (must have #)" unless path.match(/.#./)
-        controller, action = path.split('#')
+    def time_taken
+      completed_at - started_at if completed_at && started_at
+    end
 
-        controller_object = begin
-          Kernel.const_get("#{controller.classify}Jobs").new
-        rescue NameError
-          raise ArgumentError, "Invalid controller"
+private
+
+    def mark_as_started
+      update_attributes(:started_at       => Time.now)
+    end
+
+    def mark_employee
+      require 'socket'
+      update_attributes(:employee_host    => Socket.gethostname,
+                        :employee_pid     => Process.pid)
+    end
+
+    def mark_exception(exception)
+      update_attributes(:status => 'error', :error_message => exception.message, :error_backtrace => exception.backtrace)
+    end
+
+    def mark_as_completed
+      self.status ||= 'success'
+      self.completed_at = Time.now
+      self.save
+    end
+
+      class << self
+        def call_path(path, *args)
+          require 'active_support'
+
+          raise ArgumentError, "Invalid path (must have #)" unless path.match(/.#./)
+          controller, action = path.split('#')
+
+          controller_object = begin
+            Kernel.const_get("#{controller.classify}Jobs").new
+          rescue NameError
+            raise ArgumentError, "Invalid controller"
+          end
+
+          raise ArgumentError, "Invalid path action" unless controller_object.respond_to?(action)
+
+          controller_object.send(action, *args)
         end
 
-        raise ArgumentError, "Invalid path action" unless controller_object.respond_to?(action)
-
-        controller_object.send(action, *args)
-      end
-
-      def pending_paths
-        self.pending.except(:order).select('DISTINCT path').collect(&:path)
+        def pending_paths
+          self.pending.except(:order).select('DISTINCT path').collect(&:path)
+        end
       end
     end
   end
